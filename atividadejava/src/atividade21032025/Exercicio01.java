e42474355b1923cee8859644146516b3bd6ede88
BrunaBandeira2609 <bandeirab940@gmail.com>
2025-03-28 17:17:53 -0300

    ok

--- atividadejava/src/atividade21032025/Exercicio01.java
+++ atividadejava/src/atividade21032025/Exercicio01.java
@@ -1,17 +1,10 @@
 package atividade21032025;
 
+public class Exercicio01 {
 
-
-
-
-
-
-
-
-
-
-{
-    }public static void main(String[] args) throws Exception {
- System.out.println("Meu nome é Bruna");
-
-}   
+    public static void main(String[] args) {
+        System.out.println("O primeiro programa a gente nunca esquece");
+        System.out.println("Me chamo Bruna Bandeira");
+        System.out.println("Quebrei a maldição");
+    }
+}
--- atividadejava/src/atividade21032025/Exercicio02.java
+++ atividadejava/src/atividade21032025/Exercicio02.java
@@ -1,15 +1,10 @@
-package Atividade21032025;
+package atividade21032025;
 
 public class Exercicio02 {
     public static void main(String[] args) {
-        System.out.println("Teste");
-        System.out.println("Teste");
-        System.out.println("Teste");
-        System.out.println("Teste");
-        System.out.println("Teste");
-        System.out.println("Teste");
-        System.out.println("Teste");
-        System.out.println("Teste");
-        System.out.println("Teste");
+        System.out.println("Bruna dos Santos Bandeira");
+        System.out.println("Rua Botanico Kuhlmann-Eugenio Schneider");
+        System.out.println("Cep:89167-015 numero:(47)99926-4301");
+        
     }
 }
