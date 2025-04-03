0a977958322beaf4cd47e676bb84ddf5545b1d00
BrunaBandeira2609 <bandeirab940@gmail.com>
2025-04-03 11:42:26 -0300

    ok

---
+++ atividadejava/src/atividade21032025/Exercicio1115.java
@@ -0,0 +1,18 @@
+package atividade21032025;
+
+public class Exercicio1115 {
+    public static void main(String[] args) {
+        System.out.println("       *  ");
+        System.out.println("      ***  ");
+        System.out.println("     *****  ");
+        System.out.println("    *******  ");
+        System.out.println("   *********   ");
+        System.out.println("  ***********  ");
+        System.out.println(" *************  ");
+        System.out.println("*************** ");
+        System.out.println("       **     ");
+        System.out.println("       **     ");
+        System.out.println("      ****    ");
+
+    }
+}
