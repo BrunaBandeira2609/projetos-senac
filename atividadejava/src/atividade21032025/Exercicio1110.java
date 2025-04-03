8c1cd5fe1f02e3d92d2f7299af726ae8b0205f38
BrunaBandeira2609 <bandeirab940@gmail.com>
2025-04-03 19:00:23 -0300

    ok

---
+++ atividadejava/src/atividade21032025/Exercicio1110.java
@@ -0,0 +1,18 @@
+package atividade21032025;
+
+public class Exercicio1110 {
+    public static void main(String[] args) {
+        System.out.println("CADASTRO DO CLIENTE");
+
+        System.out.println("0 - Fim");
+        System.out.println("1 - Incluir");
+        System.out.println("2 - Altera");
+        System.out.println("3 - Exclui");
+        System.out.println("4 - Consulta");
+        
+        System.out.println("Opção: _");
+
+    }
+
+    
+}
