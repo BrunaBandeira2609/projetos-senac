42a9df51b0cf52d5c3a2a952233c3a4291b3635d
BrunaBandeira2609 <bandeirab940@gmail.com>
2025-04-12 17:25:41 -0300

    OK

--- atividadejava/src/atividade04042025/Exercicio06.java
+++ atividadejava/src/atividade04042025/Exercicio06.java
@@ -4,27 +4,23 @@ public class Exercicio06 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Você é maior de idade? responda SIM ou NÃO:");
-        String resposta1 = scanner.nextLine().trim().toUpperCase();
-        boolean maiorDeIdade = resposta1.equals("SIM");
+        String resposta = scanner.nextLine().trim().toUpperCase();
 
-        System.out.println("Você possui RG? responda SIM ou NÃO:");
-        String resposta2 = scanner.nextLine().trim().toUpperCase();
-        boolean possuiRG = resposta2.equals("SIM");
-
-        System.out.println(maiorDeIdade? "A pessoa é maior de idade." : "A pessoa é menor de idade");
-        System.out.println(possuiRG? "A pessoa possui RG." : "A pessoa não possui RG");
-
-        if (maiorDeIdade == possuiRG) {
-            if (maiorDeIdade) {
-                System.out.println("Ambas as respostas sào VERDADEIRAS");
-            } else {
-                System.out.println("Ambas as respostas são FALSAS.");
-            } else {
-                System.out.println("As respostas são diferentes(Uma é verdadeira e a outra é falsa)");
-            }
-            scanner.close();
+        boolean maiorDeIdade; 
+        
+        if (resposta.equals("SIM")){ 
+           maiorDeIdade = true;
+        } else {
+          maiorDeIdade = false;  
         }
-    
+        if (maiorDeIdade){ 
+            System.out.println("A pessoa é maior de idade.");
+        } else { 
+            System.out.println("A pessoa é menor de idade");
+        }
+        scanner.close();
 
+    }
 }
